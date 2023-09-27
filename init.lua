98f42758e39594ca4b72f5b25f13e9cf50d5e5ed
CaolanCode <cmacmahon3@gmail.com>
2023-09-27 17:12:23 +0100

    update init requires

--- init.lua
+++ init.lua
@@ -1,6 +1,1 @@
-require("config.lazy")
-require("config.keymaps")
-require("config.options")
-require("plugins.treesitter")
-require("plugins.telescope")
-require("plugins.lualine")
+require("config")
