5d5d0c9d5545ff8266e8eabb10b75adfddf78508
CaolanCode <cmacmahon3@gmail.com>
2023-10-09 21:52:18 +0100

    add formatter

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,36 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				html = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				lua = { "stylua" },
+				cpp = { "clang_format" },
+				terraform = { "terraform_fmt" },
+				rust = { "rustfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end, { desc = "Format on save or in range" })
+	end,
+}
