388a299b53cb69e3cf531da61680f272b3fcec90
CaolanCode <cmacmahon3@gmail.com>
2024-01-29 09:22:02 +0000

    add angular language server to mason

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -30,6 +30,7 @@ return {
 				"lua_ls",
 				"emmet_ls",
 				"terraformls",
+				"angularls",
 			},
 			-- auto-install configured servers (with lspconfig)
 			automatic_installation = true, -- not the same as ensure_installed
