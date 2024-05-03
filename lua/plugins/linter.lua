d8a1157e3231ea0a011fb3dafa6f698787e61552
CaolanCode <cmacmahon3@gmail.com>
2024-05-03 10:00:07 +0100

    change linter to eslint

--- lua/plugins/linter.lua
+++ lua/plugins/linter.lua
@@ -7,7 +7,7 @@ return {
 
 		lint.linters_by_ft = {
 			typescript = { "eslint_d" },
-			javascript = { "eslint_d" },
+			javascript = { "eslint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
